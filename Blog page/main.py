9ba114ecfb504890e763b41f2e5331e134c7ded9
Ashmi thakur <132152337+Ashmithakur29@users.noreply.github.com>
2024-07-02 16:07:55 +0530

    Update main.py

--- Blog page/main.py
+++ Blog page/main.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 import requests
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+OWN_EMAIL = "your_email"
+OWN_PASSWORD ="your_password"
+
 
 app = Flask(__name__)
 
@@ -17,9 +24,27 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+@app.route("/olderPost")
+def olderPost():
+    return render_template("olderpost.html")
 
 
 @app.route("/post/<int:index>")
